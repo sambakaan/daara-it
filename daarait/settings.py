143db558c48aed8b0b5eee77a9b78e21e00162e7
xarala <xarala@vmi247396.contaboserver.net>
2019-03-16 00:38:14 +0100

    chnage form server

--- daarait/settings.py
+++ daarait/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,"live-static", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR,  "media")
 
 # Custom user
 AUTH_USER_MODEL = 'accounts.User'
---
+++ static/frontend/js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  console.log("Ready to work!");
+
+  // dynamic date
+  const date = new Date();
+  document.querySelector('.year').innerHTML = date.getFullYear();
+
+  // partner
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+
+  // lightbox2 
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  });
+
+  // carousel home
+  $('#galleryCarousel').carousel({
+    interval: 3000
+  });
+
+  // gallery
+  $(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+
+  wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function (box) {
+      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+  });
+  wow.init();
+  document.getElementById('moar').onclick = function () {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+  };
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+  $('#back-to-top').tooltip('show');
+
+});
